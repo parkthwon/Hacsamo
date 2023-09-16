2e324008151e84e95f55aa1e31f355aefc53143e
Jihwan Byeon <wkdghk24@gmail.com>
2023-09-16 15:10:30 +0900

    [feat] Player synchronization
    - Player prefab에 charactor controller, photon transform view 컴포넌트 추가
    - 본인이 생성한 플레이어만 이동 가능
    - 하나의 화면에서 플레이어 이동 동기화
    - PhotonNetwork.serialization을 60으로 변경

---
+++ Assets/BJH/01_Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class GameManager : MonoBehaviourPun
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.SerializationRate = 60f;
+
+        // 나의 플레이어 생성
+        PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/BJH/01_Scripts/PlayerMove.cs
+++ Assets/BJH/01_Scripts/PlayerMove.cs
@@ -1,21 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 // wasd키로 플레이어를 움직인다.
-public class PlayerMove : MonoBehaviour
+public class PlayerMove : MonoBehaviourPun
 {
     public float speed = 5f;
+
+    // camera
+    public Transform cam;
+
+    // charactor controller
+    public CharacterController cc;
 
     // Start is called before the first frame update
     void Start()
     {
-                
+        cc = GetComponent<CharacterController>();
+
+        // 내가 생성한 플레이어 일때만 카메라 활성화하기
+        if (photonView.IsMine)
+        {
+            cam.gameObject.SetActive(true);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        // 내가 생성한 플레이어가 아니면 update()를 탈출
+        if (photonView.IsMine == false) 
+        {
+            return;        
+        }
+
         // input
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
