228bd7a974251cf57c9ad3c9016167e478c2fc16
Jihwan Byeon <wkdghk24@gmail.com>
2023-09-22 09:32:37 +0900

    [feat] change datat type float to int
    - 포톤 데이터를 보내는 횟수를 60f에서 60으로 타입을 변경했습니다.

--- Assets/BJH/01_Scripts/GameManager.cs
+++ Assets/BJH/01_Scripts/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : MonoBehaviourPun
     // Start is called before the first frame update
     void Start()
     {
-        PhotonNetwork.SerializationRate = 60f;
+        PhotonNetwork.SerializationRate = 60;
 
         // 나의 플레이어 생성
         PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity);
